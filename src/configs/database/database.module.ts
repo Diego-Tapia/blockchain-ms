b778dda71e1b0f953caba8ac7a70b5711b1d0c09
DESKTOP-FDSVPNE\Usuario <you@example.com>
2021-12-16 10:35:39 -0300

    fix bd

--- src/configs/database/database.module.ts
+++ src/configs/database/database.module.ts
@@ -8,18 +8,10 @@ import configs from '../environments/configs';
 @Module({
   imports: [
     MongooseModule.forRootAsync({
-      useFactory: (configService: ConfigType<typeof configs>): MongooseModuleOptions => {
-        const { engine, name, host, user, pass, port } = configService.database;
-        return {
-          uri: `${engine}://${host}:${port}`,
-          user,
-          pass,
-          dbName: name,
-          //replicaSet: "rs0",
-        };
-      },
-      inject: [configs.KEY],
-    }),
+      useFactory: () => ({
+        uri: process.env.URI,
+      }),
+    })
   ],
 })
-export class DatabaseModule { }
+export class DatabaseModule {}
